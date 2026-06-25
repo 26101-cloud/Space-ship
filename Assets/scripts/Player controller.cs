98f2d14e063357f69c4f1cd800047a1572ae0135
26101-cloud <26101@bodin2.ac.th>
2026-06-25 18:36:05 +0700

    97t98

---
+++ Assets/scripts/Player controller.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Playercontroller : MonoBehaviour
+{
+
+    public float speed = 8f;
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+
+    public Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = 0f; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float move = Input.GetAxisRaw("Horizontal");
+        rb.linearVelocity = new Vector2(move * speed, 0f);
+
+        if (transform.position.x < -3f)
+            transform.position = new Vector2(3f, transform.position.y);
+        else if (transform.position.x > 3f)
+            transform.position = new Vector2(-3f, transform.position.y);
+    }
+}
